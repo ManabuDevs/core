de8c953c2b91f2bd6e999ead32feade91e071f13
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-04-11 22:18:15 -0500

    [ADD] agregado models basico para users

---
+++ users/domain/user.go
@@ -0,0 +1,23 @@
+package users
+
+type user struct {
+	id
+	nroSocio
+	nombres
+	apellidos
+	tipoIdentificacion
+	Nacionalidad
+	FechaNacimiento
+	Edad
+	FechaIngreso
+	actividadEconomica
+	Provincia
+	Canton
+	Parroquia
+	Direccion
+	genero
+	estadoCivil
+	Telefono
+	Instruccion
+	Otros
+}
