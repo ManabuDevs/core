ffd7dcf5a17cf230b307692a400473036242bef6
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-04-17 22:40:26 -0500

    [MOD][ADD] modificado server, agregado en main serverRun

--- infrastructure/server/server.go
+++ infrastructure/server/server.go
@@ -4,26 +4,25 @@ import "github.com/gin-gonic/gin"
 
 var (
 	engineGin = gin.New
-	run       = startServer
+	run       = NewServer
 )
 
-func NewServer(server *gin.engine) {
+func NewServer(server *gin.Engine) {
 	//definir si es dev || prod || QA
 	server.Run(":3000")
 }
 
-func startServer() *gin.Engine {
+func StartServer() *gin.Engine {
 	//Create server
 	serverHTTP := engineGin()
 	serverHTTP.Use(gin.Recovery())
 
 	//set routes
+	//setRoutes(serverHTTP)
 	run(serverHTTP)
 	return serverHTTP
 }
 
-setRoutes(router *gin.Engine){
+/*setRoutes(router *gin.Engine){
 	router.GET("/health",fmt.Println("health") )
-	
-}
-
+}*/
