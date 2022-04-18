7ae49c2951f7b27aaaa6553ddedc63f114166ed5
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-04-17 21:33:32 -0500

    [MOD][ADD] modificado para agregar draft de funcion de crear usuario

--- users/http/permissions.go
+++ users/http/permissions.go
@@ -1,7 +1,14 @@
 package users
 
 /*users functions*/
-func PermissionCreate()
+func PermissionCreate() {
+	//leer datos de entrada
+	//procesar los datos de entrada
+	//validar token
+	//validar estructura correctas de entrada
+	//llamar a funcion repository
+	//Devolver error || exito (formatos json)
+}
 func PermissionUpdate()
 func PermissionDelete()
 
