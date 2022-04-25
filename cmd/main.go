744af46ee23166cb9751d291c2359539b7d723ea
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-04-24 22:51:27 -0500

    [MOD] modificado main para pruebas con bd

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+
+	//dbconfig "sabasy/infrastructure/database"
+	"database/sql"
+	"log"
+	"os"
 	"sabasy/infrastructure/server"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var start = server.StartServer
 
 func main() {
+	//a := dbconfig.InstanceDB().GetConnect()
+	//fmt.Println(a)
+	file, err := os.Create("sqlite-database.db") // Create SQLite file
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	log.Println("sqlite-database.db created")
+
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
+	defer sqliteDatabase.Close()
+
 	start().Run()
 }
