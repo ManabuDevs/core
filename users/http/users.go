373ba9c077b088a74e0512ffcb234aa538fc01a1
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-04-30 22:37:46 -0500

    [MOD] se modifico users para contener pruenas se modifico model para contener json

--- users/http/users.go
+++ users/http/users.go
@@ -1,7 +1,9 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
+	domain "sabasy/users/domain"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,11 +11,17 @@ import (
 /*users functions*/
 func UserCreate(c *gin.Context) {
 
+	var requestBody domain.User
+
+	if err := c.BindJSON(&requestBody); err != nil {
+		fmt.Println(err.Error())
+	}
+
 	/*c.JSON(http.StatusOK, gin.H{
 		"hello": c.Query("id"),
 		"test":  c.PostForm("test"),
 	})*/
-	c.JSON(http.StatusOK, c.Request.Body)
+	c.JSON(http.StatusOK, requestBody)
 }
 
 /*func UserUpdate()
