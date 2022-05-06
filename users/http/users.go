cb3d08aeaf881400575ef8f498f6dee543ec1af4
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-05-05 22:53:22 -0500

    [MOD][ADD] modificado estructra para agregar services para servir de gategay entre ports y repos

--- users/http/users.go
+++ users/http/users.go
@@ -3,27 +3,29 @@ package users
 import (
 	"fmt"
 	"net/http"
+	"sabasy/infrastructure/dbconfig"
 	"sabasy/internal/dto"
 	domain "sabasy/users/domain"
-	usersPorts "sabasy/users/ports"
+	usersRepo "sabasy/users/repo"
+	usersService "sabasy/users/services"
 
 	"github.com/gin-gonic/gin"
 )
 
-type userRepositories struct {
-	userRepos usersPorts.UserRepositories
-}
-
 /*users functions*/
 func UserCreate(c *gin.Context) {
-
 	var requestBody domain.User
 	var response dto.Request
+
+	repo := usersRepo.NewUserRepository(dbconfig.InstanceDB().GetConnect())
+	service := usersService.NewUserService(repo)
 
 	if err := c.BindJSON(&requestBody); err != nil {
 		fmt.Println(err.Error())
 	}
 
+	service.CreateUser(&requestBody)
+	//usersPorts.UserRepositories.CreateUser(requestBody)
 	/*c.JSON(http.StatusOK, gin.H{
 		"hello": c.Query("id"),
 		"test":  c.PostForm("test"),
---
+++ users/services/users.go
@@ -0,0 +1,24 @@
+package users
+
+import (
+	domain "sabasy/users/domain"
+	usersPorts "sabasy/users/ports"
+)
+
+type serviceUser struct {
+	repoUser usersPorts.UserRepositories
+}
+
+func NewUserService(repo usersPorts.UserRepositories) *serviceUser {
+	return &serviceUser{
+		repoUser: repo,
+	}
+}
+
+func (s *serviceUser) CreateUser(user *domain.User) (*domain.User, error) {
+	return s.repoUser.CreateUser(user)
+}
+
+func (s *serviceUser) GetMassiveCl() ([][]string, error) {
+	return s.repoUser.GetUsers()
+}
