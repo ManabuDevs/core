b8bbe781219f6e82985c4598b1c43f05cb34ecfa
Marlon Cepeda <marlons.cepeda@gmail.com>
2022-05-06 23:30:39 -0500

    [MOD] modificado querys para agregar userrsGet

--- internal/db-schema/querys/users.go
+++ internal/db-schema/querys/users.go
@@ -1,4 +1,5 @@
 package querys
 
 const UsersGet = `select * from users`
+const UserGet = `select * from users where id = $1`
 const UsersInsert = `insert into users (name, username, password, groupid) values (?,?,?,?)`
